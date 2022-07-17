b4f082f5fa54f879901ce2fdaf48149e39610146
allen-tao <taoqingqiu@gmail.com>
2022-07-17 16:33:09 +0800

    build: improve rollup config

---
+++ rollup.config.js
@@ -0,0 +1,54 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import path from 'path';
+import autoprefixer from 'autoprefixer';
+import postcss from 'rollup-plugin-postcss';
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+const deps = Object.keys(pkg.dependencies);
+const vue = require('rollup-plugin-vue');
+
+export default [
+  {
+    input: path.resolve(__dirname, 'packages/index.ts'),
+    output: [
+      {
+        format: 'es',
+        file: pkg.main,
+      },
+    ],
+    plugins: [
+      nodeResolve(),
+      vue(),
+      typescript({
+        tsconfigOverride: {
+          compilerOptions: {
+            declaration: true,
+          },
+          include: [
+            'packages/**/*',
+            'typings/shims-vue.d.ts',
+          ],
+          exclude: [
+            'node_modules',
+          ],
+        },
+        abortOnError: false,
+      }),
+      postcss({
+        plugins: [autoprefixer],
+        // inject: true,
+        extract: true,
+        minimize: true,
+        sourceMap: false,
+        extensions: ['.css'],
+      }),
+      terser(),
+    ],
+    external(id) {
+      return /^vue/.test(id)
+        || deps.some((k) => new RegExp(`^${k}`).test(id));
+    },
+  },
+];
